31f29dbe053950b0008d9c17c30d73c9ad93c665
darrida <darrida.py@gmail.com>
2022-12-19 22:53:44 -0600

    Added __all__ to __init__.py

--- streamlit_login_auth_ui/__init__.py
+++ streamlit_login_auth_ui/__init__.py
@@ -1,3 +1,5 @@
 from streamlit_login_auth_ui._widgets import Login
 from streamlit_login_auth_ui._widgets import cookies
 from streamlit_login_auth_ui._backwards_compatible import __login__
+
+__all__ = ['Login', 'cookies']
