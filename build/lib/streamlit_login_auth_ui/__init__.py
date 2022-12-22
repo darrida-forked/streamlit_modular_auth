d771b68642db1756d8fb363b1c42d8f035c93030
darrida <darrida.py@gmail.com>
2022-12-21 21:58:13 -0600

    removed build files; updated gitignore

--- build/lib/streamlit_login_auth_ui/__init__.py
+++
@@ -1,6 +0,0 @@
-from streamlit_login_auth_ui._widgets import Login
-from streamlit_login_auth_ui._widgets import cookies
-from streamlit_login_auth_ui import protocols
-from streamlit_login_auth_ui._legacy import __login__
-
-__all__ = ['Login', 'cookies', 'protocols']
